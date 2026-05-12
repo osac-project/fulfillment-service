a460d183c0ebaaa591d0888b327cf9afa7011a99
Adrien Gentil <agentil@redhat.com>
2026-05-12 17:17:14 +0200

    feat(05-03): add --catalog-item flag with mutual exclusivity and deprecation warning
    - Register --catalog-item flag on create cluster command (no short alias)
    - Add Cobra-level MarkFlagsMutuallyExclusive and MarkFlagsOneRequired
    - Emit deprecation warning to stderr when --template is used
    - Reject --template-parameter with --catalog-item
    - Extract pull secret and SSH key resolution before catalog-item/template branch
    - Add speculative catalog-item code path with TODO(OSAC-704) marker
    - Add Ginkgo unit tests for flag registration and validation

---
+++ internal/cmd/cli/create/cluster/create_cluster_cmd_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright (c) 2025 Red Hat Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
+*/
+
+package cluster
+
+import (
+	. "github.com/onsi/ginkgo/v2/dsl/core"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Create cluster flag registration", func() {
+	It("should register --catalog-item flag", func() {
+		cmd := Cmd()
+		flag := cmd.Flags().Lookup("catalog-item")
+		Expect(flag).NotTo(BeNil())
+		Expect(flag.Usage).To(ContainSubstring("Catalog item"))
+	})
+
+	It("should still register --template flag", func() {
+		cmd := Cmd()
+		flag := cmd.Flags().Lookup("template")
+		Expect(flag).NotTo(BeNil())
+	})
+
+	It("should register --catalog-item without a short flag", func() {
+		cmd := Cmd()
+		flag := cmd.Flags().Lookup("catalog-item")
+		Expect(flag).NotTo(BeNil())
+		Expect(flag.Shorthand).To(BeEmpty())
+	})
+
+	It("should keep -t as shorthand for --template", func() {
+		cmd := Cmd()
+		flag := cmd.Flags().Lookup("template")
+		Expect(flag).NotTo(BeNil())
+		Expect(flag.Shorthand).To(Equal("t"))
+	})
+})
+
+var _ = Describe("Create cluster flag validation", func() {
+	It("should return error when both --catalog-item and --template are set", func() {
+		cmd := Cmd()
+		cmd.SetArgs([]string{"--catalog-item", "cat-001", "--template", "tpl-001", "--name", "test"})
+		err := cmd.Execute()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("if any flags in the group"))
+		Expect(err.Error()).To(ContainSubstring("catalog-item"))
+		Expect(err.Error()).To(ContainSubstring("template"))
+	})
+
+	It("should return error when neither --catalog-item nor --template is set", func() {
+		cmd := Cmd()
+		cmd.SetArgs([]string{"--name", "test"})
+		err := cmd.Execute()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("at least one of the flags"))
+		Expect(err.Error()).To(ContainSubstring("catalog-item"))
+		Expect(err.Error()).To(ContainSubstring("template"))
+	})
+})
